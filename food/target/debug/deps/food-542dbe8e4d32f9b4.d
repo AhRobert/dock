28276e68f434d2916996b2f2149cae3ec8a2477b
AhRobert <Tauqeer.Ahmed@edu.rtu.lv>
2024-04-23 13:22:35 +0300

    1st commit

---
+++ food/target/debug/deps/food-542dbe8e4d32f9b4.d
@@ -0,0 +1,5 @@
+C:\Users\AhTau\.rustup\dock\food\target\debug\deps\libfood-542dbe8e4d32f9b4.rmeta: src\main.rs
+
+C:\Users\AhTau\.rustup\dock\food\target\debug\deps\food-542dbe8e4d32f9b4.d: src\main.rs
+
+src\main.rs:
---
+++ food/target/debug/deps/food-de1c6ae07c09eb77.d
@@ -0,0 +1,5 @@
+C:\Users\AhTau\.rustup\dock\food\target\debug\deps\libfood-de1c6ae07c09eb77.rmeta: src\main.rs
+
+C:\Users\AhTau\.rustup\dock\food\target\debug\deps\food-de1c6ae07c09eb77.d: src\main.rs
+
+src\main.rs:
